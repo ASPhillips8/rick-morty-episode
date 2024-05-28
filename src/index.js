77c94de27126a70c0f1279f1b956eadcb3ec5a63
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-05-28 19:18:59 -0400

    Create character card with mouse over

--- src/index.js
+++ src/index.js
@@ -1,34 +1,65 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("episode-form")
+  const thumbCharacter = document.getElementById("character-bar")
+  const characterSpecs = document.getElementById("preview-container")
 
-    const form = document.getElementById("episode-form")
-    const thumbCharacter = document.getElementById("character-bar")
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-    form.addEventListener("submit", (event) => {
-      event.preventDefault();
+    const episodeInput = document.getElementById("season-episode").value
+    const url = `https://rickandmortyapi.com/api/episode/?episode=${episodeInput}`
 
-      const episodeInput = document.getElementById("season-episode").value
-      const url = `https://rickandmortyapi.com/api/episode/?episode=${episodeInput}`
+    thumbCharacter.innerText = ""
 
-      thumbCharacter.innerText = ""
+    fetch(url)
+    .then((response) => response.json())
+    .then((episodeData) => {
+      let characters = episodeData.results[0].characters
+      characters.forEach((characterUrl) => renderCharacters(characterUrl))
+    })
+  })
 
-      fetch(url)
-      .then((response) => response.json())
-      .then((episodeData) => {
-        let characters = episodeData.results[0].characters
-        characters.forEach((characterUrl) => renderCharacters(characterUrl))
+  function renderCharacters(characterUrl) {
+    fetch(characterUrl)
+    .then((response) => response.json())
+    .then((character) => {
+      const img = document.createElement("img")
+
+      img.src = `${character.image}`
+      img.alt = `${character.name}`
+      img.classList.add("thumb-img")
+      thumbCharacter.appendChild(img)
+      img.addEventListener("mouseover", (event) => {showCharacter(character)
+        console.log(event.target)
+
       })
     })
+  }
+  function showCharacter(character) {
 
-    function renderCharacters(characterUrl) {
-      fetch(characterUrl)
-      .then((response) => response.json())
-      .then((character) => {
-        const img = document.createElement("img")
+    characterSpecs.textContent = ""
 
-        img.src = `${character.image}`
-        img.alt = `${character.name}`
-        img.classList.add("thumb-img")
-        thumbCharacter.appendChild(img)
-      })
-    }
+    const span = document.createElement("span")
+    const h2 = document.createElement("h2")
+    const statusH4 = document.createElement("h4")
+    const speciesH4 = document.createElement("h4")
+    const bigImg = document.createElement("img")
+    const button = document.createElement("button")
+
+    bigImg.src = `${character.image}`
+    bigImg.id = "enlarged-image"
+    h2.textContent = `Name: ${character.name}`
+    h2.id = "char-name"
+    statusH4.textContent = `Status: ${character.status}`
+    speciesH4.textContent = `Species: ${character.species}`
+    button.textContent = "GET ALL EPISODES"
+
+    span.appendChild(bigImg)
+    span.appendChild(h2)
+    h2.appendChild(statusH4)
+    h2.appendChild(speciesH4)
+    characterSpecs.appendChild(span)
+    characterSpecs.appendChild(button)
+  }
+
 })
